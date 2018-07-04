84298f451a5f0441f4d144b30dbbca20f2fbb5f0
Erin Hales <mcbridee093@gmail.com>
2018-07-04 17:52:08 -0600

    final commit

--- server/index.js
+++ server/index.js
@@ -1,21 +1,108 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get('/callback', (req, res) => {
+app.get('/auth/callback', (req, res) => {
+  exchangeCodeForAccessToken()
+    .then(exchangeAccessTokenForUserInfo)
+    .then(fetchAuth0AccessToken)
+    .then(fetchGitHubAccessToken)
+    .then(setGitTokenToSession)
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    })
 
-  // Code below
+  function exchangeCodeForAccessToken() {
+    const { REACT_APP_AUTH0_CLIENT_ID: client_id, AUTH0_CLIENT_SECRET: client_secret } = process.env;
+    const payload = {
+      client_id,
+      client_secret,
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      redirect_uri: `http://${req.headers.host}/callback`,
+    };
+    return axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload);
+  }
 
-})
+  function exchangeAccessTokenForUserInfo(accessTokenResponse) {
+    const { access_token } = accessTokenResponse.data;
+    return axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo?access_token=${access_token}`);
+  }
+
+  function fetchAuth0AccessToken(userInfoResponse) {
+    req.session.user = userInfoResponse.data;
+    const {
+      AUTH0_API_CLIENT_ID: client_id,
+      AUTH0_API_CLIENT_SECRET: client_secret,
+      REACT_APP_AUTH0_DOMAIN: domain,
+    } = process.env;
+    const payload = {
+      grant_type: 'client_credentials',
+      client_id,
+      client_secret,
+      audience: `https://${domain}/api/v2/`,
+    };
+    return axios.post(`https://${domain}/oauth/token`, payload);
+  }
+
+  function fetchGitHubAccessToken(auth0AccessTokenResponse) {
+    const options = {
+      headers: {
+        authorization: `Bearer ${auth0AccessTokenResponse.data.access_token}`,
+      },
+    };
+    return axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/users/${req.session.user.sub}`, options);
+  }
+
+  function setGitTokenToSession(gitHubAccessTokenResponse) {
+    req.session.gitHubAccessToken = gitHubAccessTokenResponse.data.identities[0].access_token;
+    res.redirect('/');
+  }
+});
+
+app.put('/api/star', (req, res) => {
+  const { gitUser, gitRepo } = req.query;
+  axios
+    .put(`https://api.github.com/user/starred/${gitUser}/${gitRepo}?access_token=${req.session.gitHubAccessToken}`)
+    .then(() => {
+      res.end();
+    })
+    .catch(console.log);
+});
+
+app.delete('/api/star', (req, res) => {
+  const { gitUser, gitRepo } = req.query;
+  axios
+    .delete(`https://api.github.com/user/starred/${gitUser}/${gitRepo}?access_token=${req.session.gitHubAccessToken}`)
+    .then(() => {
+      res.end();
+    })
+    .catch(console.log);
+});
 
 app.get('/api/user-data', (req, res) => {
-  res.status(200).json(req.session.user)
-})
+  res.status(200).json(req.session.user);
+});
 
-app.get('/api/logout', (req, res) => {
+app.post('/api/logout', (req, res) => {
   req.session.destroy();
   res.send('logged out');
-})
+});
 
 const port = 4000;
-app.listen(port, () => { console.log(`Server listening on port ${port}`); });
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
